b3d2c1d3302650f252430d6b004fb2cf39e871f5
Chiaki <Chiaki530@outlook.com>
2019-04-25 22:13:12 +0800

    add encoder

---
+++ code/encoder.py
@@ -0,0 +1,131 @@
+'''
+Created on 04/04/2019
+@auther: Jiaxin
+'''
+from gensim.models import Doc2Vec
+import os
+import re
+import codecs
+import json
+from datetime import datetime
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.decomposition import PCA
+import string
+import pandas as pd
+
+data_path = "../data/"
+result_path = "../vecs/"
+model_path = '../apnews_dbow/doc2vec.bin'
+
+
+class Encoder:
+    def __init__(self, name):
+        self.path = data_path + name
+        self.name = name
+        self.corpus = []
+        self.dates = None
+        self.ids = None
+        self.vecs = None
+
+    def read_from_json(self):
+        print("Read data from corpus of json...")
+        filepaths = [os.path.join(self.path, i)
+                     for i in os.listdir(self.path)]
+        for i, path in enumerate(filepaths):
+            if i % 1000 == 0:
+                print('Adding {} docs'.format(i))
+            fp = codecs.open(path, 'r', 'utf-8')
+            news = json.loads(fp.read())
+            # text = news['full_text']
+            text = news['title']
+            self.corpus.append(text)
+            date = datetime.strptime(news['date_publish'][:10], '%Y-%m-%d')
+            if i == 0:
+                self.dates = [date]
+                self.ids = [int(re.findall('\d+', path)[0])]
+            else:
+                self.dates = np.append(self.dates, date)
+                self.ids = np.append(self.ids, int(re.findall('\d+', path)[0]))
+            fp.close()
+        self.corpus = np.array(self.corpus)
+
+    def read_from_txt(self):
+        print("Read data from corpus of txt...")
+        filepaths = [os.path.join(self.path, i)
+                     for i in os.listdir(self.path)]
+        for i, path in enumerate(filepaths):
+            if i % 10000 == 0:
+                print('Adding {} docs'.format(i))
+            fp = open(path, 'r')
+            text = fp.read()
+            # text = news['full_text']
+            self.corpus.append(text)
+            if i == 0:
+                self.ids = [int(re.findall('\d+', path)[0])]
+            else:
+                self.ids = np.append(self.ids, int(re.findall('\d+', path)[0]))
+            fp.close()
+        self.corpus = np.array(self.corpus)
+
+    def read_from_csv(self):
+        print("Read data from csv...")
+        csv_data = pd.read_csv(self.path+'.csv')
+        self.dates = csv_data[['date']].values.flatten()
+        for i in range(self.dates.shape[0]):
+            self.dates[i] = datetime.strptime(
+                str(self.dates[i][:10]), '%Y-%m-%d')
+        self.ids = csv_data[['id']].values.flatten()
+        self.corpus = csv_data[['title']].values.flatten()
+        print(self.corpus[:3])
+        del csv_data
+
+    def doc2vec300(self, doc, model):
+        return model.infer_vector(doc.split())
+
+    def encode(self, mode, model_path=None, max_features=None):
+        if mode == 'tfidf':
+            print("Encode corpus using tfidf...")
+            vectorizer = CountVectorizer(
+                stop_words='english')
+            X = vectorizer.fit_transform(self.corpus)
+            transformer = TfidfTransformer()
+            tfidf = transformer.fit_transform(X)
+            del self.corpus
+            del X
+            pca = PCA(n_components=0.95)
+            pca.fit(tfidf.toarray())
+            tfidf = pca.transform(tfidf.toarray())
+            print(tfidf.shape)
+            self.vecs = np.hstack(
+                (self.ids.reshape((-1, 1)), np.array(tfidf)))
+        if mode == 'doc2vec':
+            print("Encode corpus using doc2vec...")
+            model = Doc2Vec.load(model_path)
+
+            self.vecs = np.empty((self.corpus.shape[0], 300))
+            for i in range(self.corpus.shape[0]):
+                if i % 1000 == 0:
+                    print('Process {} docs'.format(i))
+                # raw_text = self.corpus[i].split('-')[0]
+                doc_vec = self.doc2vec300(self.corpus[i], model)
+                self.vecs[i, :] = doc_vec
+            self.vecs = np.hstack(
+                (self.ids.reshape((-1, 1)), self.vecs))
+
+    def encode_and_save(self, mode, model_path=None, path=result_path, max_features=None):
+        self.encode(mode, model_path, max_features)
+        print("Saving docs and dates...")
+        np.save('{}{}_docs.npy'.format(result_path, self.name), self.vecs)
+        if not self.dates is None:
+            np.save('{}{}_dates.npy'.format(
+                result_path, self.name), self.dates)
+
+
+if __name__ == "__main__":
+    encoder = Encoder("text_log_mailonline")
+    # encoder.read_from_csv()
+    encoder.read_from_json()
+    encoder.encode_and_save('doc2vec', model_path)
+    # encoder.encode_and_save('tfidf', max_features=5000)
--- code/indexer.py
+++ code/indexer.py
@@ -5,50 +5,55 @@ Created on 03/09/2019
 import os
 from whoosh.index import create_in, open_dir
 from whoosh.fields import Schema, TEXT, ID, STORED, DATETIME
+from whoosh.formats import Frequency
 import sys
 import json
 import codecs
 from whoosh.analysis import StemmingAnalyzer
 from datetime import datetime
 
+
 def get_schema():
     '''
     Schema definition: title(title), path(as ID), url(as ID), content(indexed
-    but not stored),textdata (stored text content), pubtime(publish time), 
+    but not stored),textdata (stored text content), pubtime(publish time),
     modtime(time of last modification)
     '''
-    ana = StemmingAnalyzer() 
-    return Schema(title=TEXT(stored=True),path=ID, url=ID(stored=True),\
-              content=TEXT(analyzer=ana),textdata=TEXT(stored=True),\
-              pubtime=DATETIME(stored=True),modtime=STORED)
+    ana = StemmingAnalyzer()
+    return Schema(title=TEXT(stored=True), path=ID, url=ID(stored=True),
+                  content=TEXT(analyzer=ana), textdata=TEXT(stored=True),
+                  pubtime=DATETIME(stored=True), modtime=STORED)
+
 
 def add_doc(writer, path):
-    fp = codecs.open(path,'r','utf-8')
+    fp = codecs.open(path, 'r', 'utf-8')
     print('Adding:', path)
     news = json.loads(fp.read())
     text = news['full_text']
     title = news['title']
     url = news['url']
-    date = datetime.strptime(news['date_publish'][:10],'%Y-%m-%d')
+    date = datetime.strptime(news['date_publish'][:10], '%Y-%m-%d')
     modtime = os.path.getmtime(path)
-    writer.add_document(title=title, path=path, url=url,\
-        content=text,textdata=text, pubtime=date,modtime=modtime)
-    fp.close()  
+    writer.add_document(title=title, path=path, url=url,
+                        content=text, textdata=text, pubtime=date, modtime=modtime)
+    fp.close()
 
-def clean_index(root):   
+
+def clean_index(root):
     schema = get_schema()
     if not os.path.exists("indexdir"):
         os.mkdir("indexdir")
     # Creating a index writer to add document as per schema
-    ix = create_in("indexdir",schema)
+    ix = create_in("indexdir", schema)
     writer = ix.writer()
     print('Clean index, start adding...')
-    filepaths = [os.path.join(root,i) for i in os.listdir(root)]
+    filepaths = [os.path.join(root, i) for i in os.listdir(root)]
     for path in filepaths:
         add_doc(writer, path)
     print('Clean index, finish add, now commiting...')
     writer.commit()
     print('Finish commit')
+
 
 def incremental_index(root):
     ix = open_dir('indexdir')
@@ -71,13 +76,14 @@ def incremental_index(root):
                     writer.delete_by_term('path', indexed_path)
                     to_index.add(indexed_path)
         print('Incremental index, now adding...')
-        filepaths = [os.path.join(root,i) for i in os.listdir(root)]
-        for path in filepaths:  
+        filepaths = [os.path.join(root, i) for i in os.listdir(root)]
+        for path in filepaths:
             if path in to_index or path not in indexed_paths:
                 add_doc(writer, path)
         print('Incremental index, finish add, now commiting...')
         writer.commit()
-        print('Finish commit')          
+        print('Finish commit')
+
 
 def indexer(root, clean=True):
     if clean:
@@ -85,5 +91,25 @@ def indexer(root, clean=True):
     else:
         incremental_index(root)
 
+
+def test():
+    from whoosh import scoring
+    ix = open_dir('indexdir')
+    with ix.searcher(weighting=scoring.TF_IDF()) as searcher:
+        docnum = searcher.document_numbers()
+        for num in docnum:
+            if num > 5:
+                break
+            # print([doc for doc in searcher.vector_as(
+            #     "frequency", num, "title")])
+            kws = [(kw, score)
+                   for kw, score in searcher.key_terms([num], "title", numterms=10)]
+            print(kws)
+    print()
+
+
 root = "./data/text_log"
-indexer(root)
+
+if __name__ == "__main__":
+    indexer(root)
+    test()
